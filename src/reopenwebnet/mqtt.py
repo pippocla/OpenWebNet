ff63e1cf0143b32c9da2b5656db3deb67dfcb810
Karel Vervaeke <karel.vervaeke@conundra.eu>
2021-09-22 11:48:27 +0200

    Removed leading slash from mqtt topic name. Leading slashes aren't recommended

--- src/reopenwebnet/mqtt.py
+++ src/reopenwebnet/mqtt.py
@@ -6,7 +6,7 @@ import paho.mqtt.client as mqtt
 from reopenwebnet import messages
 from reopenwebnet.client import OpenWebNetClient
 
-MQTT_LIGHT_COMMAND_PATTERN = re.compile('/openwebnet/1/(\\d+)/cmd')
+MQTT_LIGHT_COMMAND_PATTERN = re.compile('openwebnet/1/(\\d+)/cmd')
 
 class MqttBridge:
     def __init__(self, config):
@@ -56,7 +56,7 @@ class MqttBridge:
             # TODO: handle other 'who' types, allow registering transformations (to allow configuring different topic and payload)
             if isinstance(msg, messages.NormalMessage):
                 if msg.who == '1':
-                    topic = f"/openwebnet/{msg.who}/{msg.where}/state"
+                    topic = f"openwebnet/{msg.who}/{msg.where}/state"
                     logging.debug('publishing to %s: %s'%(topic, msg))
                     self.mqtt.publish(topic, msg.what)
 
@@ -68,7 +68,7 @@ def _create_mqtt_client(mqtt_config):
 
     def on_connect(client, b, c, d):
         logging.debug('mqtt connected %s/%s/%s/%s', client, b, c, d)
-        client.subscribe('/openwebnet/1/+/cmd')
+        client.subscribe('openwebnet/1/+/cmd')
 
     client.on_connect = on_connect
     client.connect(mqtt_config.host, port=mqtt_config.port)
